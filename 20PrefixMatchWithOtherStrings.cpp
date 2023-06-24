fce46ac59831644e87c317ea534eea055d321958
AmolTandon07 <119669191+AmolTandon07@users.noreply.github.com>
2023-06-24 23:46:48 +0530

    Create 20PrefixMatchWithOtherStrings.cpp

---
+++ 20PrefixMatchWithOtherStrings.cpp
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{   
+public:
+    int klengthpref(string arr[], int n, int k, string str){    
+        int i,count=0;
+        string find,temp,s;
+        find=str.substr(0,k);
+        if(k>str.length())
+        return 0;
+        for(i=0;i<n;i++)
+        {
+            
+            s=arr[i];
+            temp=s.substr(0,k);
+            if(temp==find)
+            count++;
+        }
+        return count;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        string arr[n];
+        for(int i=0;i<n;i++)
+        {
+            string s;
+            cin>>arr[i];
+            
+        }
+        int k;
+        cin>>k;
+        string str;
+        cin>>str;
+        
+        Solution ob;
+        cout << ob.klengthpref(arr, n, k, str) << endl;
+    }
+    return 0; 
+} 
+
+// } Driver Code Ends

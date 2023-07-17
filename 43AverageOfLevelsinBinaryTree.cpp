968f1d2b1231967d731ddfeea8f716f82800d3bf
AmolTandon07 <119669191+AmolTandon07@users.noreply.github.com>
2023-07-17 22:06:10 +0530

    Create 43AverageOfLevelsinBinaryTree.cpp

---
+++ 43AverageOfLevelsinBinaryTree.cpp
@@ -0,0 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<double> averageOfLevels(TreeNode* root) {
+        queue<TreeNode*>q;
+        q.push(root);
+        q.push(NULL);
+        double sum=0,count=0;
+        vector<double>ans;
+        while(!q.empty())
+        {
+            TreeNode* temp=q.front();
+            q.pop();
+            if(temp==NULL)
+            {
+                double avg;
+                avg=sum/count;
+                ans.push_back(avg);
+                sum=0;
+                count=0;
+                if(!q.empty())
+                {
+                    q.push(NULL);
+                }
+            }
+            else
+            {
+                sum=sum+temp->val;
+                count++;
+                if(temp->left)
+                {
+                    q.push(temp->left);
+                }
+                if(temp->right)
+                {
+                    q.push(temp->right);
+                }
+            }
+        }
+        return ans;
+    }
+};

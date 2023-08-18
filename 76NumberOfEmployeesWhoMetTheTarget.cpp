90f11691ed4da73ef2f6eca85a1e297ad0918b91
AmolTandon07 <119669191+AmolTandon07@users.noreply.github.com>
2023-08-18 23:51:33 +0530

    Create 76NumberOfEmployeesWhoMetTheTarget.cpp

---
+++ 76NumberOfEmployeesWhoMetTheTarget.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int numberOfEmployeesWhoMetTarget(vector<int>& hours, int target) {
+       int i;
+       int count=0;
+       for(i=0;i<hours.size();i++)
+       {
+
+           if(hours[i]>=target)
+           {
+               count++;
+           }
+       } 
+
+       return count; 
+    }
+};

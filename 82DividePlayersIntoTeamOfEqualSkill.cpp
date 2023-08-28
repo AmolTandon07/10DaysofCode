829f40bf7dd51a197f71c08fc6a6cdfcf6352ce5
AmolTandon07 <119669191+AmolTandon07@users.noreply.github.com>
2023-08-28 22:52:18 +0530

    Update 82DividePlayersIntoTeamOfEqualSkill.cpp

--- 82DividePlayersIntoTeamOfEqualSkill.cpp
+++ 82DividePlayersIntoTeamOfEqualSkill.cpp
@@ -1,1 +1,28 @@
+class Solution {
+public:
+    long long dividePlayers(vector<int>& skill) {
+        int l,r;
+        long long sum=0;
+        sort(skill.begin(),skill.end());
+        l=0;
+        r=skill.size()-1;
+        int target=skill[0]+skill[skill.size()-1];
+        while(l<=r)
+        {
+            if(skill[l]+skill[r]==target)
+            {
+                sum=sum+(skill[l]*skill[r]);
+                l++;
+                r--;
+            }
+            else
+            {
+                sum=-1;
+                break;
+            }
+            
+        }
+        return sum;
 
+    }
+};
